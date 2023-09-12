87ca57439e8e1987991c6433a64b17279494c29e
taina-arj <arj.taina@gmail.com>
2023-09-12 15:54:08 -0300

    add all

---
+++ javascript/lgnjs.js
@@ -0,0 +1,12 @@
+function darkMode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+  element.classList.toggle("fa-thumbs-down");
+}
+function goRegister() {
+  location.href = "registerpage.html";
+}
+
+function goLogin() {
+  location.href = "loginpage.html";
+}
